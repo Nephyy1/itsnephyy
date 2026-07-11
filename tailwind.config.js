0912f9aa4af98b43b80f4ef10c5df1f608cf8b53
Nephyy <142471741+Nephyy1@users.noreply.github.com>
2026-07-11 12:26:35 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,21 +8,9 @@ module.exports = {
   ],
   theme: {
     extend: {
-      animation: {
-        'gradient-x': 'gradient-x 15s ease infinite',
-        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-      },
-      keyframes: {
-        'gradient-x': {
-          '0%, 100%': {
-            'background-size': '200% 200%',
-            'background-position': 'left center'
-          },
-          '50%': {
-            'background-size': '200% 200%',
-            'background-position': 'right center'
-          },
-        },
+      fontFamily: {
+        sans: ['var(--font-jakarta)'],
+        serif: ['var(--font-playfair)'],
       },
     },
   },
